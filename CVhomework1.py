848bd0dbff13ed57eede26330e46fa88a44d5e2c
raphael.cardoso.f@gmail.com <raphael.cardoso.f@gmail.com>
2019-01-20 00:22:57 -0500

    Started part I

--- CVhomework1.py
+++ CVhomework1.py
@@ -1,4 +1,21 @@
 from scipy.io import loadmat
+import cv2, os
+import numpy as np
 
-data = loadmat("filters/leung_malik_filter.mat")
-print(data)
+def part1():
+    myImages = []
+    imagesPath = os.path.abspath(os.path.join("images"))
+    filters = os.path.abspath(os.path.join("filters"))
+    leung_malik = loadmat(os.path.join(filters,"leung_malik_filter.mat"))
+    for i in os.listdir(imagesPath):
+        img = cv2.imread(os.path.join(imagesPath,i),0)
+        img = cv2.resize(img, (100, 100))
+        myImages.append(img)
+    np.convolve(img,leung_malik["F"][0])
+    # for i in myImages:
+    #     cv2.imshow('image', i)
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+        
+
+part1()
