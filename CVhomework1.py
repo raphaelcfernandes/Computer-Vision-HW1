5fc9c9c578c70554785b260d8c15af5dbed952bf
raphael.cardoso.f@gmail.com <raphael.cardoso.f@gmail.com>
2019-01-22 18:23:30 -0500

    Fixed filter selection

--- CVhomework1.py
+++ CVhomework1.py
@@ -1,21 +1,41 @@
 from scipy.io import loadmat
+from scipy import ndimage
 import cv2, os
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 def part1():
     myImages = []
     imagesPath = os.path.abspath(os.path.join("images"))
     filters = os.path.abspath(os.path.join("filters"))
-    leung_malik = loadmat(os.path.join(filters,"leung_malik_filter.mat"))
-    for i in os.listdir(imagesPath):
-        img = cv2.imread(os.path.join(imagesPath,i),0)
-        img = cv2.resize(img, (100, 100))
+    leung_malik = loadmat(os.path.join(filters, "leung_malik_filter.mat"))["F"]
+    images = os.listdir(imagesPath)
+    images.sort()
+    for i in images:
+        img = cv2.imread(os.path.join(imagesPath, i), 0)
+        img = cv2.resize(img, (600, 600))
         myImages.append(img)
-    np.convolve(img,leung_malik["F"][0])
-    # for i in myImages:
-    #     cv2.imshow('image', i)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
-        
-
+    if not os.path.exists("part1_plots"):
+        os.mkdir("part1_plots")
+    for i in range(48):
+        fig, axs = plt.subplots(2, 4)
+        axs[0, 1].axis("off")
+        axs[0, 0].imshow(leung_malik[:,:, i])
+        axs[0, 0].set_title("Filter")
+        axs[0, 2].imshow(ndimage.convolve(myImages[0],leung_malik[:,:,i]))
+        axs[0, 2].set_title(images[0])
+        axs[0, 3].imshow(ndimage.convolve(myImages[1],leung_malik[:,:,i]))
+        axs[0, 3].set_title(images[1])
+        axs[1, 0].imshow(ndimage.convolve(myImages[2],leung_malik[:,:,i]))
+        axs[1, 0].set_title(images[2])
+        axs[1, 1].imshow(ndimage.convolve(myImages[3],leung_malik[:,:,i]))
+        axs[1, 1].set_title(images[3])
+        axs[1, 2].imshow(ndimage.convolve(myImages[4],leung_malik[:,:,i]))
+        axs[1, 2].set_title(images[4])
+        axs[1, 3].imshow(ndimage.convolve(myImages[5],leung_malik[:,:,i]))
+        axs[1, 3].set_title(images[5])
+        name = 'plot_filter_'+str(i)+'.png'
+        fig.savefig(os.path.abspath(os.path.join("part1_plots", name)))
+        plt.close(fig)
 part1()
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Computer-Vision-HW1
 
 https://people.cs.pitt.edu/~kovashka/cs2770_sp19/hw1/index.html
+# for i in range(leung_malik.shape[2]):
+    #     cv2.imshow('image', ndimage.convolve(myImages[2],leung_malik[:,:,0]))
+    #     cv2.waitKey(2000)
+    #     cv2.destroyAllWindows()
+    #     break
+Part I
+    5
+    
